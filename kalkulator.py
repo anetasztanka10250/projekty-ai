6d8ebebb0409b7dfea0f5d98c7dce3e23b424967
Aneta10250 <aneta.sztanka10250@gmail.com>
2026-05-03 00:52:34 +0200

    Dodaje kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+print("=== Kalkulator ===")
+
+a = float(input("Podaj pierwszą liczbę: "))
+b = float(input("Podaj drugą liczbę: "))
+
+print("Dodawanie:", a + b)
+print("Odejmowanie:", a - b)
+print("Mnożenie:", a * b)
+print("Dzielenie:", a / b)
