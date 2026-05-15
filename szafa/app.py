07abf7bff27915f5f0c753e0a3f3ab63d7bbd4fb
Aneta10250 <aneta.sztanka10250@gmail.com>
2026-05-15 22:11:48 +0200

    fix host binding

--- szafa/app.py
+++ szafa/app.py
@@ -351,4 +351,4 @@ def eksport():
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(os.path.join(BASE_DIR, 'data'), exist_ok=True)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
